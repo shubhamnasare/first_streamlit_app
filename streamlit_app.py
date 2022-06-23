22d0a8f78ee5e5f1e038ed32b77beee0c77a5605
Shubham Nasare <108076984+shubhamnasare@users.noreply.github.com>
2022-06-23 20:17:13 +0530

    Update streamlit_app.py
    added fruits_selected and fruits_to_show

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,11 @@ my_fruit_list  = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/d
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 #streamlit.multiselect("Pick some fruits : ",list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits : ",list(my_fruit_list.index),['Avocado','Strawberries'])
+#streamlit.multiselect("Pick some fruits : ",list(my_fruit_list.index),['Avocado','Strawberries'])
 
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits : ",list(my_fruit_list.index),['Avocado','Strawberries'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+
+
